b77dbb52fe9694811e8412964a2f0642a519905f
griseeminence <p.kolt080@gmail.com>
2024-02-17 13:19:56 +0100

    random_250_search with connect db. Good response

--- cinemator/core.py
+++ cinemator/core.py
@@ -1,9 +1,11 @@
 import logging
 import requests
+import random
 
 from dotenv import dotenv_values
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler, ConversationHandler
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, \
+    CallbackQueryHandler, ConversationHandler
 
 from cinemator.database import init_db, get_movies_to_watch, get_favorite_movies, add_movie_to_watch, add_favorite_movie
 
@@ -19,7 +21,6 @@ logging.basicConfig(
 
 url_random = 'https://api.kinopoisk.dev/v1.4/movie/random'
 
-
 headers = {
     "accept": "application/json",
     "X-API-KEY": KINOPOISK_TOKEN
@@ -34,6 +35,7 @@ init_db()
 # Определяем состояния для диалога
 ENTER_MOVIE = 0
 ADD_TO_LISTS = 1
+ADD_RANDOM_TO_LISTS = 0
 
 
 # Отдельный декоратора для логгирования
@@ -68,8 +70,9 @@ async def start(update, context):
     """
     Стартовая клавиатура. Четыре кнопки и приветствие.
     Каждая кнопка перехватывается отдельным обработчиком MessageHandler
+    Добавлен случай обработки callback_query для вызова с кнопок (использую для возврата пользователя в меню)
     """
-    print(f'Запустил функцию старт')
+    print(f'Зашёл в функцию старт')
     print(update)
     print(context)
     if update.message:
@@ -83,30 +86,61 @@ async def start(update, context):
         if data == "start":
             keyboard = get_keyboard()
             await update.callback_query.message.reply_text(
-                "Привет! Давай начнем. Что я могу для тебя сделать?",
+                "Хорошо, начнём с самого начала. Что я могу для тебя сделать?",
                 reply_markup=keyboard
             )
-        # Добавьте обработку других данных коллбэка здесь
     else:
         print("Обновление не содержит ни сообщения, ни данных коллбэка.")
-    print(f'вышел из функции старт')
+    print(f'Вышел из функции старт')
 
 
 # TODO: Random Movie BLOCK
 async def random_movie(update, context):
     # TODO: Выдаёт рандомный тайтл. У фильмов часто пустые поля. Придумать, как фильтровать рандом по списку топ-250
-    keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Главное меню", callback_data="start")]
-    ])
-    url_random_search = 'https://api.kinopoisk.dev/v1.4/movie/random'
-    name = requests.get(url_random_search, headers=headers).json()['name']
-    id = requests.get(url_random_search, headers=headers).json()['id']
-    year = requests.get(url_random_search, headers=headers).json()['year']
-    description = requests.get(url_random_search, headers=headers).json()['description']
-    poster = requests.get(url_random_search, headers=headers).json()['poster']
-    rating = requests.get(url_random_search, headers=headers).json()['rating']['kp']
-    message_text = f'{name}\n{year}\n{description}\n{rating}\n{poster}'
+
+    # url_random_250 = 'https://api.kinopoisk.dev/v1.4/movie?page=1&limit=250&selectFields=id&selectFields=top250&sortField=top250&sortType=-1'
+    url_random_250 = 'https://api.kinopoisk.dev/v1.4/movie?page=1&limit=250&selectFields=id&selectFields=top250&selectFields=name&selectFields=description&selectFields=year&selectFields=rating&selectFields=poster&selectFields=genres&sortField=top250&sortType=-1'
+    response = requests.get(url_random_250, headers=headers).json()['docs']
+    id_with_250 = []
+    for movie in response:
+        if isinstance(movie['top250'], int):  # Проверяем, является ли значение целым числом
+            id_with_250.append(movie['id'])  # Если да, добавляем id в список
+    test_id = id_with_250
+    print(test_id)
+    print(len(test_id))
+    random_250 = random.randrange(0, 250)
+
+    name = requests.get(url_random_250, headers=headers).json()['docs'][random_250]['name']
+    genres = requests.get(url_random_250, headers=headers).json()['docs'][random_250]['genres'][0]['name']
+    id = requests.get(url_random_250, headers=headers).json()['docs'][random_250]['id']
+    year = requests.get(url_random_250, headers=headers).json()['docs'][random_250]['year']
+    description = requests.get(url_random_250, headers=headers).json()['docs'][random_250]['description']
+    poster = requests.get(url_random_250, headers=headers).json()['docs'][random_250]['poster']['url']
+    rating = requests.get(url_random_250, headers=headers).json()['docs'][random_250]['rating']['kp']
+    message_text = f'Название: {name}\nГод: {year}\nОписание: {description}\nРейтинг: {rating}\nЖанр: {genres}\nПостер: {poster}'
     await context.bot.send_message(chat_id=update.effective_chat.id, text=message_text)
+
+    context.user_data['movie_info'] = {
+        'name': name,
+        'description': description,
+        'year': year,
+        'genres': genres,
+        'rating': rating
+    }
+
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton("Save to Favorite Movie", callback_data="add_to_list_favorite_movie")],
+            [InlineKeyboardButton("Save to Movie to watch", callback_data="add_to_list_movie_to_watch")],
+            [InlineKeyboardButton("Menu", callback_data="start")],
+        ],
+    )
+    await update.callback_query.message.reply_text(
+        "Хочешь сохранить фильм?",
+        reply_markup=keyboard
+    )
+
+    return ADD_TO_LISTS
 
 
 # TODO: END Random Movie BLOCK
@@ -118,6 +152,8 @@ async def favorite_movie(update, context):
     movie_list = get_favorite_movies(user_id=user.id)
     formatted_movie_list = "\n\n".join([f"{movie[0]}\n{movie[1]}\n{movie[2]}" for movie in movie_list])
     await context.bot.send_message(chat_id=update.effective_chat.id, text=formatted_movie_list)
+
+
 # TODO: END Favorite Movie BLOCK
 
 # TODO: Movie to watch BLOCK
@@ -126,6 +162,8 @@ async def movie_to_watch(update, context):
     movie_list = get_movies_to_watch(user_id=user.id)
     formatted_movie_list = "\n\n".join([f"{movie[0]}\n{movie[1]}\n{movie[2]}" for movie in movie_list])
     await context.bot.send_message(chat_id=update.effective_chat.id, text=formatted_movie_list)
+
+
 # TODO: END Movie to watch BLOCK
 
 
@@ -140,28 +178,15 @@ async def start_movie_search(update, context):
     return ENTER_MOVIE
 
 
-
-
-async def ask_movie_name(update, context):
-    """Задаем вопрос о имени."""
-    await update.callback_query.message.reply_text("2Как называется фильм, который тебя интересует?")
-
-    # Ожидаем ответ пользователя
-    return ENTER_MOVIE
-
-
 async def save_movie_name(update, context):
     """Сохраняем ответ пользователя."""
     user_id = update.message.from_user.id
     movie_for_search = update.message.text
-
-    # Сохраняем ответ пользователя
     context.user_data['movie'] = movie_for_search
-
     await update.message.reply_text(
-        f"Спасибо, {update.effective_chat.username}! Выполняю поиск фильма: {movie_for_search}")
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=movie_for_search)
-
+        f"Спасибо, {update.effective_chat.username}! Выполняю поиск фильма: {movie_for_search}"
+    )
+    # TODO: Добавить к упл сортировку по рейтингу
     url_name_search = f'https://api.kinopoisk.dev/v1.4/movie/search?page=1&limit=10&query={movie_for_search}'
     name = requests.get(url_name_search, headers=headers).json()['docs'][0]['name']
     genres = requests.get(url_name_search, headers=headers).json()['docs'][0]['genres'][0]['name']
@@ -195,6 +220,7 @@ async def save_movie_name(update, context):
 
     return ADD_TO_LISTS
 
+
 async def add_to_lists(update, context):
     user = update.effective_user
     query = update.callback_query
@@ -223,27 +249,18 @@ async def add_to_lists(update, context):
                 rating=movie_info['rating']
             )
         await query.message.reply_text(f'Movie {movie_info["name"]} successfully added to Movie to watch list!')
-#TODO: не работает вызов start
     else:
         await start(update, context)
 
     # Завершаем разговор
     return ConversationHandler.END
 
-#test
+
 # TODO: END Find Movie BLOCK
 
 # TODO: UNKNOWN BLOCK
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # Ниже - код для теста: сохраняет в БД отправленное пользователем сообщение
-    # Ниже - код для теста: сохраняет в БД отправленное пользователем сообщение
     user = update.effective_user
-    if user:
-        name = user.first_name
-    else:
-        name = 'Anonym'
-    text = update.effective_message.text
-
 
     keyboard = get_keyboard()
     await update.message.reply_text(
@@ -257,15 +274,13 @@ async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # TODO: BUTTONS BLOCK
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Обработка нажатий на кнопки."""
-    user = update.effective_user
     query = update.callback_query
-    if query.data == "ask_movie_name":
-        await ask_movie_name(update, context)
-    elif query.data == "movie_to_watch":
+    # if query.data == "ask_movie_name":
+    #     await ask_movie_name(update, context)
+    if query.data == "movie_to_watch":
         await movie_to_watch(update, context)
     elif query.data == "favorite_movie":
         await favorite_movie(update, context)
-
 
 
 # TODO: END BUTTONS BLOCK
@@ -274,7 +289,8 @@ if __name__ == '__main__':
     application = ApplicationBuilder().token(TG_TOKEN).build()
     start_handler = CommandHandler('start', start)
     start_movie_search_handler = CallbackQueryHandler(start_movie_search, pattern=r'^ask_movie_name$')
-    conversation_handler = ConversationHandler(
+    random_movie_handler = CallbackQueryHandler(random_movie, pattern=r'^random_movie$')
+    search_conversation_handler = ConversationHandler(
         entry_points=[start_movie_search_handler],
         states={
             ENTER_MOVIE: [MessageHandler(filters.TEXT & (~filters.COMMAND), save_movie_name)],
@@ -282,15 +298,20 @@ if __name__ == '__main__':
         },
         fallbacks=[],
     )
-    random_movie_handler = CallbackQueryHandler(random_movie, pattern=r'^random_movie$')
+    random_conversation_handler = ConversationHandler(
+        entry_points=[random_movie_handler],
+        states={
+            ADD_TO_LISTS: [CallbackQueryHandler(add_to_lists)]
+        },
+        fallbacks=[],
+    )
+
     favorite_movie_handler = CallbackQueryHandler(favorite_movie, pattern=r'^favorite_movie$')
     movie_to_watch_handler = CallbackQueryHandler(movie_to_watch, pattern=r'^movie_to_watch$')
-
-
     unknown_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), unknown)
 
-
-    application.add_handler(conversation_handler)
+    application.add_handler(search_conversation_handler)
+    application.add_handler(random_conversation_handler)
     application.add_handler(start_handler)
     application.add_handler(start_movie_search_handler)
     application.add_handler(random_movie_handler)
